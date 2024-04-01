11211e8d60c1c361decdb3bfab3b4c691c501f09
king-of-state <131938395+king-of-state@users.noreply.github.com>
2024-04-01 18:05:33 +0530

    Update ILendingPool.sol

--- Multiplier-Finance-main/MCL-FlashloanDemo/blob/main/contracts/interfaces/ILendingPool.sol
+++ Multiplier-Finance-main/MCL-FlashloanDemo/blob/main/contracts/interfaces/ILendingPool.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.5.0;
+pragma solidity >=0.8.10;
  
 contract IPancakeCalleeA {
     function pancakeCall02(address sender, uint amount0, uint amount1, bytes calldata data) external;
